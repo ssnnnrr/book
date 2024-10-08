4eb1408846a92417747800672d60aa9389f0d393
Asus <Asus@DESKTOP-F2RLVQ4>
2024-10-08 19:57:16 +0300

    Добавьте файлы проекта.

---
+++ book/Classes/DBConnectionClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using book.Datadase;
+
+namespace book.Classes
+{
+    internal class DBConnectionClass
+    {
+        public static BookEntities DatabaseConnection = new BookEntities();
+    }
+}
---
+++ book/Datadase/BookPartialClass.cs
@@ -0,0 +1,42 @@
+﻿using book.Classes;
+using System.Linq;
+using System.Windows.Media;
+using System;
+
+namespace book.Datadase
+{
+    public partial class Book
+    {
+        public SolidColorBrush DiscountColor
+        {
+            get
+            {
+                var maxDiscount = Classes.DBConnectionClass.DatabaseConnection.MaxDiscount.Where(c => c.MaxDiscountId == MaxDiscountId).First();
+
+                if (maxDiscount == null)
+                    return Brushes.Transparent;
+
+                if (maxDiscount.Value >= 15)
+                    return Brushes.Chartreuse;
+                else if (maxDiscount.Value >= 10)
+                    return Brushes.Yellow;
+                else if (maxDiscount.Value >= 5)
+                    return Brushes.Orange;
+                else
+                    return Brushes.Transparent;
+            }
+        }
+        public string NewPrice
+        {
+            get
+            {
+                if (ActiveDiscountId != null)
+                {
+                    string a = Convert.ToString(Price - (Price / 100 * DBConnectionClass.DatabaseConnection.ActiveDiscount.Where(c => c.ActiveDiscountId == ActiveDiscountId).First().Value));
+                    return "\t" + a;
+                }
+                else return null;
+            }
+        }
+    }
+}
